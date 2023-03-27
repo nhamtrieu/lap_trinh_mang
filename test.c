ffcefc7c5f3130a6d0032ac0f9f9bd9faa938f8b
nhamtrieu <trieunham2002@gmail.com>
2023-03-27 15:55:35 +0700

    test

--- test.c
+++ test.c
@@ -4,5 +4,7 @@ int main() {
     printf("hello world\n");
     int n;
     scanf("%d", &n);
-    printf("So ban da nhap la: %d\n", n);
+    int i = 1;
+    for(i; i <= n; i++)
+        printf("So ban da nhap la: %d\n", i);
 }
